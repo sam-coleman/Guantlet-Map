36d3ad4a65f98f66a817cb4bfc5755a771cdc5d0
sam-coleman <60279462+sam-coleman@users.noreply.github.com>
2020-04-26 18:27:13 -0400

    clearned code

--- createLidarMap.m
+++ createLidarMap.m
@@ -6,11 +6,9 @@ load('data_lidar')
 %row 1 goes with row 5, 2 with 6, 3 with 7, 4 with 8
 r_L = [r_all(:,1:end).*cos(theta_all(:, 1:end)), r_all(:,1:end).*sin(theta_all(:,1:end))]';
 
-
 %location of objects with respect to Neato frame N
 %need to subtract .084 from rows 1-4 of r_L and keep rows 5-8 the same
 r_N = [r_L(1:4, :) - .084; r_L(5:end, :)];
-
 
 %location of objects with respect to Global frame G
 %iterate through each of the 4 starting places of Neato, and find the
@@ -23,40 +21,32 @@ for n = 1:4 %n is the 4 placements of the Neato
     
     %Translation Matrix to go from Neato Frame to Global Frame
     T_GN = [1 0 x; 0 1 y; 0 0 1];
-    
-    %assign the position matrix
-    if n == 1
-        r_N_pos = [r_N(1, :); r_N(5, :); ones(1, 360)];
-    elseif n == 2 
-        r_N_pos = [r_N(2, :); r_N(6, :); ones(1, 360)];
-    elseif n == 3
-        r_N_pos = [r_N(3, :); r_N(7, :); ones(1, 360)];
-    elseif n == 4
-        r_N_pos = [r_N(4, :); r_N(8, :); ones(1, 360)];
-    end
-    
     %Rotation Matrix to go from Neato Frame to Global Frame
     R_GN = [cos_phi -sin_phi 0; sin_phi cos_phi, 0; 0 0 1];
-        
-    %make separate matrices for each position
+   
+    %assign the position matrix and make separte matrices for each starting
+    %position after doing transformations
     if n == 1
+        r_N_pos = [r_N(1, :); r_N(5, :); ones(1, 360)];
         r_N_pos1 = T_GN * R_GN * r_N_pos;
         r_N_pos1 = r_N_pos1(1:2, :);
-    elseif n == 2
+    elseif n == 2 
+        r_N_pos = [r_N(2, :); r_N(6, :); ones(1, 360)];
         r_N_pos2 = T_GN * R_GN * r_N_pos;
-        r_N_pos2 = r_N_pos2(1:2, :);
+        r_N_pos2 = r_N_pos2(1:2, :);       
     elseif n == 3
+        r_N_pos = [r_N(3, :); r_N(7, :); ones(1, 360)];
         r_N_pos3 = T_GN * R_GN * r_N_pos;
-        r_N_pos3 = r_N_pos3(1:2, :);
+        r_N_pos3 = r_N_pos3(1:2, :);       
     elseif n == 4
+        r_N_pos = [r_N(4, :); r_N(8, :); ones(1, 360)];
         r_N_pos4 = T_GN * R_GN * r_N_pos;
         r_N_pos4 = r_N_pos4(1:2, :);
     end
-    
-    %concatante into one big matrix
-    r_N_all = [r_N_pos1; r_N_pos2; r_N_pos3; r_N_pos4];
-   
 end
+
+%concatante into one big matrix
+r_N_all = [r_N_pos1; r_N_pos2; r_N_pos3; r_N_pos4];
 
 %Plot the map!!
 figure(), clf
